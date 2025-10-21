154c23de17556c1bad33e86bc37c102170d39d9f
Shubham Kumar Sah <shubham.sah2212@gmail.com>
2025-10-21 18:33:08 +0530

    update

--- wavelet_transform/practice.m
+++ wavelet_transform/practice.m
@@ -1,61 +1,40 @@
-% Fusion of Infrared and Visible Image:
-
-clc; clear all; close all;
-% 1. Creating background Mask:
+clc; clear; close all;
 
 % Load Input Images
 IR = imread('manWalkIR.jpg');
 VIS = imread('manWalkVB.jpg');
-
 figure(1)
-subplot(1, 2, 1)
-imshow(IR)
-title('Infrared Image')
-subplot(1, 2, 2)
-imshow(VIS)
-title('Visible Image')
+imshow(IR); title('Original Infrared Image');
 
 % Preprocess Infrared Image
 grayIR = rgb2gray(IR);
-grayVIS = rgb2gray(VIS);
+figure(2)
+imhist(grayIR); title('Histogram of Infrared Grayscale Image');
 
-smoothedIR = imgaussfilt(grayIR, 2); % Gaussian smoothing
-level = graythresh(smoothedIR); % Otsu threshold
+smoothedIR = imgaussfilt(grayIR, 2);  % Gaussian smoothing
+level = graythresh(smoothedIR);      % Otsu threshold
 threshold = round(level * 255);
 fprintf('Computed Otsu Threshold: %d\n', threshold);
 
 binaryMask = smoothedIR > threshold;
-binaryMask = imclose(binaryMask, strel('disk', 5)); % Fill gaps
-binaryMask = bwareaopen(binaryMask, 100); % Remove small fragments
+binaryMask = imclose(binaryMask, strel('disk', 5));   % Fill gaps
+binaryMask = bwareaopen(binaryMask, 100);             % Remove small fragments
 
-% Apply Mask to IR Image
+% Apply Mask to IR Image and VIS Image
 maskedIR = IR;
 maskedIR(repmat(~binaryMask, [1 1 3])) = 0;
-figure(2)
+figure(3)
 imshow(maskedIR); title('Masked IR Image (Auto ROI)');
 
-% Create STM and BM Masks
-stm = uint8(binaryMask) * 255;
-bm = uint8(~binaryMask) * 255;
-figure(3)
-subplot(1,2,1); imshow(stm); title('Salient Target Mask');
-subplot(1,2,2); imshow(bm); title('Background Mask');
-
-greyI = rgb2gray(IR);
-result1 = greyI .* uint8(binaryMask);
+maskedVIS = VIS;
+maskedVIS(repmat(~binaryMask, [1 1 3])) = 0;
 figure(4)
-subplot(1, 2, 1)
-imshow(result1)
-title('Salient × Infrared');
-result2 = grayVIS .* uint8(~binaryMask);
-subplot(1, 2, 2)
-imshow(result2)
-title('Background × Infrared');
+imshow(maskedVIS); title('Masked VIS Image (Auto ROI)');
 
-% 2. Applying DWT :
-% Resize visible image to match IR dimensions
-[rows, cols] = size(IR);
-VIS = imresize(VIS, [rows cols]);
+figure(5)
+imshow(binaryMask); title('Binary Mask');
+
+% apply dwt:
 
 % Convert to double precision for processing
 IR = im2double(IR);
@@ -95,7 +74,3 @@ else
     HH_fused = HH_VIS;
 end
 
-% 3. Fusion of infrared and visible:
-Fused = idwt2(LL_fused, LH_fused, HL_fused, HH_fused, 'db2');
-
-fig
