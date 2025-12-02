244642e8b5c90890c7ee73148e576659b7ef7228
Shubham Kumar Sah <shubham.sah2212@gmail.com>
2025-12-02 21:59:48 +0530

    update

--- Final_Paper/STD.m
+++ Final_Paper/STD.m
@@ -8,8 +8,8 @@ clear; clc; close all;
 % ----------------------------
 % Load images
 % ----------------------------
-ir = im2double(imread("IR_lake_g.bmp"));
-vi = im2double(imread("VIS_lake_r.bmp"));
+ir = im2double(imread("IR_meting016_g.bmp"));
+vi = im2double(imread("VIS_meting016_r.bmp"));
 
 % Convert to grayscale if needed
 if size(ir,3) > 1, ir = rgb2gray(ir); end
@@ -39,11 +39,13 @@ fused = mat2gray(fused);   % normalize
 entropy_fused = entropy(fused);
 
 % deviation
-dev = deviation_1(vi, fused);
+dev_vi = deviation_1(vi, fused);
+dev_ir = deviation_1(ir, fused);
 
 % PSNR with respect to IR and visible
-% psnr_ir = psnr(fused, ir);
 psnr_vi = psnr(fused, vi);
+psnr_ir = psnr(fused, ir);
+
 
 % Spatial Frequency (SF)
 RF = sqrt(mean(diff(fused,1,1).^2,'all'));  % row frequency
@@ -51,11 +53,12 @@ CF = sqrt(mean(diff(fused,1,2).^2,'all'));  % column frequency
 SF = sqrt(RF^2 + CF^2);
 
 % SSIM
-% ssim_ir = ssim(fused, ir);
+ssim_ir = ssim(fused, ir);
 ssim_vi = ssim(fused, vi);
 
 % Correlation Coefficient (with VI)
 corr_vi = corr2(fused, vi);
+corr_ir = corr2(fused, ir);
 
 % ----------------------------
 % Display metrics
@@ -63,11 +66,15 @@ corr_vi = corr2(fused, vi);
 fprintf('\n----- Fusion Performance Metrics -----\n');
 
 fprintf('PSNR (fused vs Visible): %.4f dB\n', psnr_vi);
+fprintf('PSNR (fused vs IR): %.4f dB\n', psnr_ir);
 fprintf('Entropy: %.4f\n', entropy_fused);
 fprintf('Spatial Frequency: %.4f\n', SF);
 fprintf('SSIM (fused vs Visible): %.4f\n', ssim_vi);
+fprintf('SSIM (fused vs IR): %.4f\n', ssim_ir);
 fprintf('Correlation Coefficient (fused vs VI): %.4f\n', corr_vi);
-fprintf('Deviation: %.4f\n', dev);
+fprintf('Correlation Coefficient (fused vs IR): %.4f\n', corr_ir);
+fprintf('Deviation (VI): %.4f\n', dev_vi);
+fprintf('Deviation (IR): %.4f\n', dev_ir);
 
 % ----------------------------
 % Visualization
